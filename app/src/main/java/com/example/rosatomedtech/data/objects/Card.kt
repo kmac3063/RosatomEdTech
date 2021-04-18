10edd8b1b660832e73a03e61c4faa85919d7bc24
BLGALEX <lepehaleha@bk.ru>
2021-04-18 14:09:26 +1000

    Добавлена сущьность теста в базу данных

--- app/src/main/java/com/example/rosatomedtech/data/objects/Card.kt
+++ app/src/main/java/com/example/rosatomedtech/data/objects/Card.kt
@@ -1,11 +1,14 @@
 package com.example.rosatomedtech.data.objects
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "card_table")
 data class Card(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
-    val imgUrl: String
+    val imgUrl: String,
+    @ForeignKey(entity = Company::class, onDelete = ForeignKey.CASCADE, parentColumns = ["id"], childColumns = ["id", "imgUrl"])
+    val company: Company
     )
